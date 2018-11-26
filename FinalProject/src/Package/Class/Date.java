321e2f1cc0b4af855bb5e702a2e314056bf3ee32
daffatahtaa <43267539+daffatahtaa@users.noreply.github.com>
2018-11-26 23:30:48 +0700

    Design aplikasi awal
    pembuatan frame home dan login

---
+++ FinalProject/src/Package/Class/Date.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Package.Class;
+
+import java.text.SimpleDateFormat;
+
+
+/**
+ *
+ * @author Expression daffa is undefined on line 12, column 14 in Templates/Classes/Class.java.
+ */
+public class Date {
+    Date date = new Date();
+    
+    
+}
