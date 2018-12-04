0451d8a4c5182c99257708c34076d318bf2e4771
firnandakusuma <firnandapratama99@gmail.com>
2018-12-04 13:33:14 +0700

    Komentar

--- FinalProject/src/Package/BaseFrame.java
+++ FinalProject/src/Package/BaseFrame.java
@@ -19,7 +19,7 @@ public class BaseFrame extends javax.swing.JFrame {
         jTextField1.setEditable(Boolean.FALSE);
         jTextField1.disable();
     }
-
+//puuulllll
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
